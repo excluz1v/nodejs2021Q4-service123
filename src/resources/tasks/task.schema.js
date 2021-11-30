50fe62a21c0cb4e26bde2b929ebcafd3c13f9394
excluz1v <bmd4work@gmail.com>
2021-11-30 23:51:28 +0600

    feat: create Task Schemes

---
+++ src/resources/tasks/task.schema.js
@@ -0,0 +1,98 @@
+const taskSchema = {
+  id: { type: 'string' },
+  title: { type: 'string' },
+  order: { type: 'number' },
+  description: { type: 'string' },
+  userId: { type: 'string' },
+};
+
+const createTaskSchema = {
+  title: 'string',
+  order: 'number',
+  description: 'string',
+  userId: 'string',
+  boardId: 'string',
+  columnId: 'string',
+};
+
+// Options for GET ALL BOARDS
+const getTaskOpts = {
+  schema: {
+    response: {
+      200: {
+        type: 'array',
+        items: {
+          type: 'object',
+          properties: taskSchema,
+        },
+      },
+    },
+  },
+};
+
+// Options for POST CREATE BOARDS
+const postTasksOpts = {
+  schema: {
+    body: {
+      type: 'object',
+      required: [
+        'title',
+        'columnId',
+        'order',
+        'description',
+        'userId',
+        'boardId',
+      ],
+      properties: createTaskSchema,
+    },
+    response: {
+      201: {
+        type: 'object',
+        properties: taskSchema,
+      },
+    },
+  },
+};
+
+// Options for GET boards/:boardId
+const getTaskByIdOpts = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: taskSchema,
+      },
+    },
+  },
+};
+
+// Options for PUT BOARD(Updates a Board by ID)
+const putTaskOpts = {
+  schema: {
+    body: {
+      type: 'object',
+      required: [
+        'title',
+        'columnId',
+        'order',
+        'description',
+        'userId',
+        'boardId',
+      ],
+      properties: createTaskSchema,
+    },
+    response: {
+      201: {
+        type: 'object',
+        properties: taskSchema,
+      },
+    },
+  },
+};
+
+module.exports = {
+  getTaskOpts,
+  postTasksOpts,
+  getTaskByIdOpts,
+  putTaskOpts,
+};
