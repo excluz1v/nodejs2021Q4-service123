c6676ef5d0bf1e5ac062d3df90c507ab41f4e6de
excluz1v <bmd4work@gmail.com>
2021-12-04 23:11:50 +0600

    refactor: delete console log

--- src/server.js
+++ src/server.js
@@ -32,7 +32,6 @@ fastify.register(taskRoutes);
 const start = async () => {
   try {
     await fastify.listen(PORT);
-    console.log(`App is running on http://localhost:${PORT}`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
